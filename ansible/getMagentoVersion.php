52c9d333407a1edae3137b83557667fb95a563ad
Navid Nadali <navidnadali@gmail.com>
2018-03-04 10:55:43 +0000

    remove old variables

--- ansible/getMagentoVersion.php
+++ ansible/getMagentoVersion.php
@@ -10,15 +10,11 @@ m2config=$vhostPath"/app/etc/env.php"
 
 # if m1
 if [ -f $m1config ]; then
-scriptFileName=$m1scriptfilename
-scriptToUse=$m1script
 echo "1"
 exit 0
 fi
 # elseif m2
 if [ -f $m2config ]; then
-scriptFileName=$m2scriptfilename
-scriptToUse=$m2script
 echo "2"
 exit 0
 fi
